3ed948c755e98c894fe70ad16402714308dbf0aa
Daniel Cortez <dany_.com@hotmail.es>
2022-03-25 22:37:43 -0500

    Se agrego codigo inicial

---
+++ app.js
@@ -0,0 +1,32 @@
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    console.log(pokeName);
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            pokeImage("./pokemon-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            document.getElementById("tipo").innerHTML = data.types[0].type.name;
+            document.getElementById("hp").innerHTML = data.stats[0].base_stat;
+            document.getElementById("ataque").innerHTML = data.stats[1].base_stat;
+            document.getElementById("defense").innerHTML = data.stats[2].base_stat;
+            document.getElementById("special-attack").innerHTML = data.stats[3].base_stat;
+            document.getElementById("special-defense").innerHTML = data.stats[4].base_stat;
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
